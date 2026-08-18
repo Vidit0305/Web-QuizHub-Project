5c6e9f5c160a7f758c22471cec408efd799020e5
Vidit Sharma <vidit.sharma0305@gmail.com>
2026-08-18 21:06:07 +0530

    Commit

--- src/main/java/com/quizhub/model/QuizResult.java
+++ src/main/java/com/quizhub/model/QuizResult.java
@@ -1,62 +1,37 @@
 package com.quizhub.model;
 
-import java.util.Map;
-
 public class QuizResult {
+
     private int totalQuestions;
     private int correctAnswers;
-    private int scorePercentage;
-    private String feedback;
-    private Map<Integer, Integer> correctOptions;
+    private int wrongAnswers;
+    private double percentage;
 
-    public QuizResult() {
-    }
+    public QuizResult(
+            int totalQuestions,
+            int correctAnswers,
+            int wrongAnswers,
+            double percentage) {
 
-    public QuizResult(int totalQuestions, int correctAnswers, int scorePercentage, String feedback, Map<Integer, Integer> correctOptions) {
         this.totalQuestions = totalQuestions;
         this.correctAnswers = correctAnswers;
-        this.scorePercentage = scorePercentage;
-        this.feedback = feedback;
-        this.correctOptions = correctOptions;
+        this.wrongAnswers = wrongAnswers;
+        this.percentage = percentage;
     }
 
     public int getTotalQuestions() {
         return totalQuestions;
     }
 
-    public void setTotalQuestions(int totalQuestions) {
-        this.totalQuestions = totalQuestions;
-    }
-
     public int getCorrectAnswers() {
         return correctAnswers;
     }
 
-    public void setCorrectAnswers(int correctAnswers) {
-        this.correctAnswers = correctAnswers;
+    public int getWrongAnswers() {
+        return wrongAnswers;
     }
 
-    public int getScorePercentage() {
-        return scorePercentage;
-    }
-
-    public void setScorePercentage(int scorePercentage) {
-        this.scorePercentage = scorePercentage;
-    }
-
-    public String getFeedback() {
-        return feedback;
-    }
-
-    public void setFeedback(String feedback) {
-        this.feedback = feedback;
-    }
-
-    public Map<Integer, Integer> getCorrectOptions() {
-        return correctOptions;
-    }
-
-    public void setCorrectOptions(Map<Integer, Integer> correctOptions) {
-        this.correctOptions = correctOptions;
+    public double getPercentage() {
+        return percentage;
     }
 }
