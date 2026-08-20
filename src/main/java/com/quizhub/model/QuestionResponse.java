cba6b257b31380c014a0e4bbc63426667bb94313
Vidit Sharma <vidit.sharma0305@gmail.com>
2026-08-20 18:54:09 +0530

    Commit

---
+++ src/main/java/com/quizhub/model/QuestionResponse.java
@@ -0,0 +1,28 @@
+package com.quizhub.model;
+public class QuestionResponse{
+    private int id;
+    private String category;
+    private String question;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String optionD;
+
+    public QuestionResponse(
+        int id;
+        String category,
+        String question,
+        String optionA,
+        String optionB,
+        String optionC,
+        String optionD){
+
+    this.id = id;
+    ths.category = category;
+    this.question = question;
+    this.optionA = optionA;
+    this.optionB = optionB;
+    this.optionC = optionC;
+    this.optionD = optionD;
+        }
+}
